93ad04ff7cc33825029a4a9f374b4279eecc6b69
fai-sas <craftedbyfaisal@gmail.com>
2024-05-16 17:14:08 +0600

    create student controller added

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+import { StudentServices } from './student.service'
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body
+    const result = await StudentServices.createStudentIntoDb(studentData)
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const StudentControllers = { createStudent }
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -2,8 +2,12 @@ import { Student } from './student.interface'
 import { StudentModel } from './student.model'
 
 const createStudentIntoDb = async (student: Student) => {
-  const result = await StudentModel.create(student)
-  return result
+  try {
+    const result = await StudentModel.create(student)
+    return result
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export const StudentServices = {
