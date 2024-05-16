c134873ddb5fb84777455e4af19677a70c23f2d7
fai-sas <craftedbyfaisal@gmail.com>
2024-05-16 18:07:18 +0600

    get all students controller added

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -4,7 +4,7 @@ import { StudentServices } from './student.service'
 const createStudent = async (req: Request, res: Response) => {
   try {
     const { student: studentData } = req.body
-    const result = await StudentServices.createStudentIntoDb(studentData)
+    const result = await StudentServices.createStudentIntoDB(studentData)
 
     res.status(200).json({
       success: true,
@@ -16,4 +16,18 @@ const createStudent = async (req: Request, res: Response) => {
   }
 }
 
-export const StudentControllers = { createStudent }
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB()
+
+    res.status(200).json({
+      success: true,
+      message: 'Students retrieved successfully',
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const StudentControllers = { createStudent, getAllStudents }
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,15 +1,17 @@
 import { Student } from './student.interface'
 import { StudentModel } from './student.model'
 
-const createStudentIntoDb = async (student: Student) => {
-  try {
-    const result = await StudentModel.create(student)
-    return result
-  } catch (error) {
-    console.log(error)
-  }
+const createStudentIntoDB = async (student: Student) => {
+  const result = await StudentModel.create(student)
+  return result
+}
+
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find()
+  return result
 }
 
 export const StudentServices = {
-  createStudentIntoDb,
+  createStudentIntoDB,
+  getAllStudentsFromDB,
 }
